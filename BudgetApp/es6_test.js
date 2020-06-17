6e0e908a6ce298ec1af8bdd95cc3b72eb3dea5b3
ljw <jwluo@cenboomh.com>
2020-06-17 17:06:35 +0800

    新增Budget文件夹

---
+++ BudgetApp/es6_test.js
@@ -0,0 +1,59 @@
+let arr = ['Jack', 'Jack', 'Kay','Kay','Zion','Zion'];
+let result = [];
+
+var selectedTags = ['A02', 'B02', 'C01'];
+
+function Person(name, age, callback){
+    this.name = name;
+    this.age = age;
+    this.callback = callback;
+}
+
+var newPerson = new Person ('Jack', 28);
+
+(function() {
+    console.log(new Person('Jack', 28, a)).name;
+    
+})()
+
+
+let count = 7;
+while (count > 0) {
+    count--;
+    console.log(count);
+}
+
+
+
+
+
+var obj = {
+    name: 'aj',
+    price: '2400',
+    a,
+}
+
+var a = function() {
+    console.log('自动触发')
+}
+
+
+
+
+
+
+
+//console.log(result);
+
+// (function() {
+//     let i = 4;
+//     do{
+//         console.log(i);
+//         i++;
+//     } while (i < 8)
+// })()
+
+
+
+
+
